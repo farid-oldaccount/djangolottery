aa9cbf47fb7cf24e6b0b4dd84c8956774299db63
Farid el Nasire <iam@faridelnasire.com>
2014-01-10 16:16:32 +0100

    Participants model + participants get saved in DB

--- djangolottery/apps/website/models.py
+++ djangolottery/apps/website/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Participants(models.Model):
+	name = models.CharField(max_length=64)
+	email = models.CharField(max_length=128)
+	bcaddress = models.CharField(max_length=34)
+	amount = models.FloatField(max_length=30)
--- djangolottery/apps/website/views.py
+++ djangolottery/apps/website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
 import json
+from models import Participants
 
 def index(request):
 	return render_to_response('website/index.html',{},context_instance=RequestContext(request))
@@ -10,9 +11,13 @@ def participate(request):
 	name = request.POST['name']
 	email = request.POST['email']
 	bcaddress = request.POST['bcaddress']
-	amount = request.POST['amount']
+	amount = request.POST['amount'].replace(',','.')
 
-
-	status = {'status': 'ok'}
+	try:
+		participant = Participants(name=name, email=email, bcaddress=bcaddress, amount=amount)
+		participant.save()
+		status = {'status': 'ok'}
+	except Exception as e:
+		status = {'status': 'error'}
 
 	return HttpResponse(json.dumps(status), content_type='application/json')
